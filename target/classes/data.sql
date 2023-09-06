87f6df1150aea7264c72cae3886dd982d88d16f6
lamine <laoufi.mohamed.lamine@gmail.com>
2023-09-06 09:30:40 +0100

    first commit

---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into fruits (fruit_name,fruit_price) values('banana',2);
+insert into fruits (fruit_name,fruit_price) values('apple',2.6);
+insert into fruits (fruit_name,fruit_price) values('peach',4);
+insert into fruits (fruit_name,fruit_price) values('graspe',5);
